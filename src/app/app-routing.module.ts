fac0bcc3abfbbc9ef8ccb7a45d7c9c0d8860157b
Mohamed Salah <mohamedsalahmasoud173@gmail.com>
2023-03-20 15:04:05 +0200

    adding auth gaurd to login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './auth.guard';
 import { LoginComponent } from './components/login/login.component';
 import { TodoInputComponent } from './components/todo-input/todo-input.component';
 
 const routes: Routes = [
   {path: '', component: LoginComponent},
-  {path: 'login', component: LoginComponent},
+  {path: 'login', component: LoginComponent ,canActivate:[AuthGuard]},
   {path: 'todos' , component : TodoInputComponent},
 ];
 @NgModule({
