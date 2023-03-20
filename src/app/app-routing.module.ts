73005bbe4f70c1d61f07fa10a7f9f593eeb5aa48
omar3ain <omar3ain@gmail.com>
2023-03-20 14:28:03 +0200

    changing the routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { LoginComponent } from './components/login/login.component';
 import { TodoInputComponent } from './components/todo-input/todo-input.component';
 
 const routes: Routes = [
+  {path: '', redirectTo: '/login', pathMatch: 'full'},
   {path: 'login', component: LoginComponent},
   {path: 'todos' , component : TodoInputComponent},
 ];
