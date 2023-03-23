bf7ac76c8570e93bd9142a79885f29cd786acbc5
Mohamed Salah <mohamedsalahmasoud173@gmail.com>
2023-03-23 16:00:30 +0200

    removing icon from deleted todo page

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -1,5 +1,4 @@
 import { Component ,Input , EventEmitter, Output} from '@angular/core';
-import { TodoService } from 'src/app/services/todo.service';
 import User from 'src/app/services/userInterface';
 import Todo from '../../services/todoInterface';
 
@@ -17,11 +16,7 @@ export class TodoItemComponent {
     isDeleted : false
   };
   @Output() returnTodo = new EventEmitter<Todo>();
-  constructor(private _todoservice:TodoService){
 
-  }
-  
-  // condition: boolean = eval("!this."+this._todoservice.showCondition); 
 
 
   changeStatus(id : number): void {
@@ -31,15 +26,13 @@ export class TodoItemComponent {
    deleteTodo(id : number){
     this.todo.status = false;
     this.todo.isDeleted = true;
-    
-    // this._todoservice.showCondition='';
-    // this._todoservice.buttonClicked.emit();
+    this.todo.isFav= false;
    }
    getTodo() {
-    // this.condition=eval(this._todoservice.showCondition);
     this.returnTodo.emit(this.todo)
    }
    favTodo(id : number){
+
     this.todo.isFav =!this.todo.isFav;
    }
 }
