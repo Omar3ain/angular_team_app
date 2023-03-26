39dcad20f0410464a8d4701b941eece28a52c8f4
omar3ain <omar3ain@gmail.com>
2023-03-26 16:50:00 +0200

    added navigator object to add battery status & internet status & os and browser

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -5,6 +5,7 @@ import { TodoService } from 'src/app/services/todo.service';
 import Todo from 'src/app/services/todoInterface';
 import User from 'src/app/services/userInterface';
 
+declare let navigator: any;
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
@@ -17,11 +18,28 @@ export class NavbarComponent implements OnInit {
   todos : Todo[]=[];
   name : string = 'random';
   user : User ={} as User;
+  browser: string = '';
+  os: string= '';
+  online = navigator.onLine;
+  batteryLevel: number = 0;
   constructor(private _activatedRoute:ActivatedRoute,private _authService: AuthService,private _router:Router,private _todoService:TodoService) {
     this.userId = Number(_activatedRoute.snapshot.params['id']);
+    window.addEventListener('online', () => this.online = true);
+    window.addEventListener('offline', () => this.online = false);
   }
+
   ngOnInit(): void {
     this.getUsers()
+    navigator.getBattery().then((battery : any )=> {
+      this.batteryLevel = battery.level * 100;
+      battery.addEventListener('levelchange', () => {
+        this.batteryLevel = battery.level * 100;
+      });
+    });
+
+    const userAgent = navigator.userAgent;
+    this.browser = this.getBrowser(userAgent);
+    this.os = this.getOS(userAgent);
   }
   getUsers() {
     this.users = this._authService.getUserInLocalStorage();
@@ -62,4 +80,37 @@ export class NavbarComponent implements OnInit {
     this._router.navigate(['/login']);
     }
 
-}
+    private getBrowser(userAgent: string): string {
+      const browsers = {
+        Chrome: /chrome/i,
+        Safari: /safari/i,
+        Firefox: /firefox/i,
+        Edge: /edg/i,
+        IE: /msie|trident/i
+      };
+      for (const browser in browsers) {
+        //@ts-ignore
+        if (browsers[browser].test(userAgent)) {
+          return browser;
+        }
+      }
+      return 'Unknown';
+    }
+  
+    private getOS(userAgent: string): string {
+      const os = [
+        { name: 'Windows', regex: /windows nt/i },
+        { name: 'Mac OS', regex: /mac os/i },
+        { name: 'Linux', regex: /linux/i },
+        { name: 'iOS', regex: /(ipad|iphone|ipod)/i },
+        { name: 'Android', regex: /android/i }
+      ];
+      for (const platform of os) {
+        if (platform.regex.test(userAgent)) {
+          return platform.name;
+        }
+      }
+      return 'Unknown';
+    }
+  }
+
--- src/app/components/todo-input/todo-input.component.ts
+++ src/app/components/todo-input/todo-input.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 import { TodoService } from 'src/app/services/todo.service';
 import User from 'src/app/services/userInterface';
 import  Todo  from '../../services/todoInterface';
@@ -20,9 +21,11 @@ export class TodoInputComponent {
 
   constructor(
     private activatedRoute: ActivatedRoute,
-    private todoService: TodoService
+    private todoService: TodoService,
+    private authService : AuthService
   ) {
     this.userId = Number(this.activatedRoute.snapshot.params['id']);
+    this.user = authService.getUserInLocalStorage().find((user : User) => user.id === this.userId)
     this.todoService.buttonClicked.subscribe(()=>{
       this.getTodos();
     })
